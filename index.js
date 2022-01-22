49965f168f80f9eeaf2343d18ef7b4d5c17ecdb1
Mihaela Valac <mvalac@student.cscc.edu>
2022-01-21 20:50:17 -0500

    Created the askQuestion function

--- index.js
+++ index.js
@@ -5,6 +5,54 @@ const table = require("console.table");
 db.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + db.threadId + "\n");
-  //askQuestions();
+  askQuestions();
 });
 
+
+const askQuestions = () =>  {
+  inquirer.prompt({
+      message: "what would you like to do?",
+      type: "list",
+      choices: [
+          "View all employees",
+          "View all departments",
+          "Add employee",
+          "Add department",
+          "Add role",
+          "Update employee role",
+          "QUIT"
+      ],
+      name: "choice"
+  }).then(answers => {
+      console.log(answers.choice);
+      switch (answers.choice) {
+          case "View all employees":
+              viewEmployees()
+              break;
+
+          case "View all departments":
+              viewDepartments()
+              break;
+
+          case "Add employee":
+              addEmployee()
+              break;
+
+          case "Add department":
+              addDepartment()
+              break;
+
+          case "Add role":
+              addRole()
+              break;
+
+          case "Update employee role":
+              updateEmployeeRole();
+              break;
+
+          default:
+              db.end()
+              break;
+      }
+  })
+}
